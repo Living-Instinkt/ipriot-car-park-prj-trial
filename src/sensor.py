738a8238e090cc1632655e7f5b0e6f2adfeb5e49
20154551 <20154551@tafe.wa.edu.au>
2025-12-04 16:27:57 +0800

    Feat(core): Create and initialise Sensor and Display classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,11 @@
 class Sensor:
+    def __init__(self, sensor_id, car_park_id, is_active = False):
+        self.id = sensor_id
+        self.car_park_id = car_park_id
+        self.is_active = is_active
+
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
     pass
